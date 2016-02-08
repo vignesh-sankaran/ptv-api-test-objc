8ec06d7b546047af8e858f2f0f479caf84ce901b
Vignesh Sankaran <developer@sankaran.io>
2016-02-08 18:06:40 +1100

    Added PtvApi files

---
+++ ptv-api-test-objc/PtvApi.h
@@ -0,0 +1,16 @@
+//
+//  PtvApi.h
+//  ptv-api-test-objc
+//
+//  Created by Vignesh Sankaran on 8/02/2016.
+//  Copyright © 2016 Vignesh Sankaran. All rights reserved.
+//
+
+#ifndef PtvApi_h
+#define PtvApi_h
+
+@interface PtvApi : NSObject
+
+@end
+
+#endif /* PtvApi_h */
---
+++ ptv-api-test-objc/PtvApi.m
@@ -0,0 +1,14 @@
+//
+//  PtvApi.m
+//  ptv-api-test-objc
+//
+//  Created by Vignesh Sankaran on 8/02/2016.
+//  Copyright © 2016 Vignesh Sankaran. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "PtvApi.h"
+
+@implementation PtvApi
+
+@end
