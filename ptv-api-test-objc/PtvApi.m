a6e095306e11d4f05ba1aacbe3924e0bb5e094a4
Vignesh Sankaran <developer@sankaran.io>
2016-02-09 18:43:26 +1100

    HMAC library is coded, no guarantee it'll work yet though...

--- ptv-api-test-objc/PtvApi.m
+++ ptv-api-test-objc/PtvApi.m
@@ -7,13 +7,24 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <CommonCrypto/CommonHMAC.h>
+#import <CommonCrypto/CommonDigest.h>
 #import "PtvApi.h"
 
 @implementation PtvApi
 
 - (NSString *)CreateHmacSignature: (NSString *)url
 {
-    return NULL;
+    NSDictionary *apiSecrets = [[NSDictionary alloc] initWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"ApiKeys" ofType:@"plist"]];
+    NSString *apiKey = [apiSecrets valueForKey:@"ApiKey"];
+    
+    unsigned char rawHmacOutput[CC_SHA1_DIGEST_LENGTH];
+    CCHmac(CC_SHA1_BLOCK_BYTES, (__bridge const void *)(apiKey), apiKey.length, (__bridge const void *)(url), url.length, rawHmacOutput);
+    
+    NSData *refinedHmacOutput = [[NSData alloc] initWithBytes:rawHmacOutput length:sizeof(rawHmacOutput)];
+    
+    return [[NSString alloc] initWithData:refinedHmacOutput encoding:NSUTF8StringEncoding];
+    
 }
 
 @end
