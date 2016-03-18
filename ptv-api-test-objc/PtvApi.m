741e01d3f2e9e40cc4b0bf61f1f838d76a599d18
Vignesh Sankaran <developer@sankaran.io>
2016-03-18 15:58:39 +1100

    Separated code that generates the full API URL

--- ptv-api-test-objc/PtvApi.m
+++ ptv-api-test-objc/PtvApi.m
@@ -37,32 +37,35 @@
     [dateFormatter setLocale:[NSLocale localeWithLocaleIdentifier:@"en_US_POSIX"]];
     [dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ssZZZZZ"];
     return [dateFormatter stringFromDate:[NSDate date]];
-    
 }
 
-- (NSData *)healthCheck
+- (NSString *)GenerateRequestUrl
 {
     NSString *baseUrl = @"http://timetableapi.ptv.vic.gov.au";
     NSString *healthCheckUrl = @"/v2/healthCheck";
     NSString *currentDateTimeInISO8601 = [self CurrentDateTimeInISO8601];
- 
+    
     NSDictionary *apiSecrets = [[NSDictionary alloc] initWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"ApiKeys" ofType:@"plist"]];
     NSString *devId = [apiSecrets valueForKey:@"DevId"];
-    
     NSString *preHmacUrl = [NSString stringWithFormat:@"%@%@%@%@%@%@", baseUrl, healthCheckUrl, @"?timestamp=", currentDateTimeInISO8601, @"&devid=", devId];
+    
     NSString *hmac = [self CreateHmacSignature:preHmacUrl];
     
     NSString *fullUrl = [NSString stringWithFormat:@"%@%@%@", preHmacUrl, @"&signature=", hmac];
+    
+    return fullUrl;
+}
+
+- (void)healthCheck
+{
+    NSString *fullUrl = [self GenerateRequestUrl];
     
     NSURLSession *apiSession = [NSURLSession sharedSession];
     NSURL *apiUrl = [NSURL URLWithString: fullUrl];
     NSMutableURLRequest *urlRequest = [NSMutableURLRequest requestWithURL:apiUrl];
     [apiSession dataTaskWithRequest:urlRequest completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-        NSLog(fullUrl);
         NSLog(@"This is a test to see what prints out here");
     }];
-    
-    return nil;
 }
 
 @end
