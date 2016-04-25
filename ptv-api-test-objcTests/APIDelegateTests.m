a1a166b49c7a8bb7a5fe571a297fa47308828b21
Vignesh Sankaran <developer@sankaran.io>
2016-04-25 17:38:40 +1000

    Removed unnecessary performance test

--- ptv-api-test-objcTests/APIDelegateTests.m
+++ ptv-api-test-objcTests/APIDelegateTests.m
@@ -55,12 +55,4 @@
     
     XCTAssert([self waitForCompletion:30.0], @"Testing to see what happens here...");
 }
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
-
 @end
